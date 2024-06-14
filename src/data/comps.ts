bc0a90b36d8d2063e0a7deb385287015a59fd7ec
Stephen Greig <steve.greig1@gmail.com>
2024-06-14 22:38:18 +0100

    Copy tweaks etc

---
+++ src/data/comps.ts
@@ -0,0 +1,14 @@
+export const comps = [
+  {
+    'slug': "volteras",
+    'company': 'Volteras',
+    'role': 'Frontend / Design Engineer',
+    'signoff': "Volteras seems like a great company on an upward trajectory, and I'd love to join your cause - if you think I'd be a good fit for your team, please reach out below."
+  },
+  {
+    'slug': "ooooby",
+    'company': 'Ooooby',
+    'role': 'Front End Engineer',
+    'signoff': "I love the concept of Ooooby and having watched the video with Pete on your About page, I can confidently say it feels like a company I'd love to work for. If you think I'd be a good fit, please reach out below."
+  },
+]
