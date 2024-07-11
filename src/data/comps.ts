b64110266b4e6c893f8ef3ea82098af5fba4306a
Stephen Greig <steve.greig1@gmail.com>
2024-07-11 23:58:43 +0100

    Sequence

--- src/data/comps.ts
+++ src/data/comps.ts
@@ -17,4 +17,10 @@ export const comps = [
     'role': 'Front End Engineer',
     'signoff': "I love the concept of Ooooby and having watched the video with Pete on your About page, I can confidently say it feels like a company I'd love to work for. If you think I'd be a good fit, please reach out below."
   },
+  {
+    'slug': "sequence",
+    'company': 'Sequence',
+    'role': 'Design Engineer',
+    'signoff': "I love the intricacy of some of the components on your website, and I can very much imagine myself building these types of features. And from everything I've read, Sequence feels like it would be a fantastic company to work for. If you think I'd be a good fit, please reach out below."
+  },
 ]
